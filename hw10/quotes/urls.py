292d54b616b934b6c8015c768227373a1b364c28
Oleksii <user@example.com>
2025-08-31 20:43:37 +0300

    authors.html

--- hw10/quotes/urls.py
+++ hw10/quotes/urls.py
@@ -6,4 +6,5 @@ app_name="quotes"
 urlpatterns = [
     path("", views.main, name="root"),
     path("<int:page>", views.main, name="root_paginate"),
+    path("author/<str:author_fullname>", views.author, name="author"),
 ]
--- hw10/quotes/views.py
+++ hw10/quotes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Author, Tag, Quote
 from django.core.paginator import Paginator
@@ -10,3 +10,7 @@ def main(request, page=1):
     paginator = Paginator(list(quotes), per_page)
     quotes_on_page = paginator.page(page)
     return render(request, 'quotes/index.html', context={'quotes' : quotes_on_page})
+
+def author(request, author_fullname):
+    author=get_object_or_404(Author, fullname=author_fullname)
+    return render(request, 'quotes/author.html', context={'author' : author})
